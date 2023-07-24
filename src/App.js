8aa7c310ea3580b93a4ebcd135bbe83ed0e42cb2
Siddhant Khurana <94681983+sidkhurana2002@users.noreply.github.com>
2023-07-24 11:02:41 +0530

    made dynamic

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+import React, { useState } from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "headphones",
+    amount: 2000,
+    date: new Date(2023, 2, 28),
+  },
+  {
+    id: "e2",
+    title: "TV",
+    amount: 30000,
+    date: new Date(2023, 3, 2),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 60000,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 10000,
+    date: new Date(2021, 5, 12),
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "headphones",
-      amount: 2000,
-      date: new Date(2023, 2, 28),
-    },
-    {
-      id: "e2",
-      title: "TV",
-      amount: 30000,
-      date: new Date(2023, 3, 2),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 60000,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 10000,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]; // This is a good practice to update the state when it depends on the previous state.
+    });
   };
 
   return (
@@ -38,7 +42,6 @@ const App = () => {
       <Expenses items={expenses} />
     </div>
   );
-}
-
+};
 
 export default App;
